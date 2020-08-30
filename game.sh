6310f2e8d7efd66264d6aa93781afa648463ee0b
M.G <magardner2010@gmail.com>
2020-08-30 14:23:40 +0300

    add main game file

---
+++ game.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NAME=${NAME-$USER}
+
+make
+
+coords=$(echo "? $NAME"|./users)
+
+if [ "$coords" = "" ] ; then
+	read -r -p "please enter your coordinates seperated by a space or a tab:" coords
+	echo "+ $NAME $coords" |./users
+fi
+
+shoot(){
+	wind=$(($RANDOM % 360))
+	echo "Wind heading: $wind degrees!"
+	read -r -p "throw heading?" heading
+	read -r -p "Throw strength?" pow
+	read -r -p "throw height (angle)?" angle
+	land=$(echo "$coords $pow $heading $angle $windi"|./parabola)
+	echo "Balloon landed at $land!"
+	for a in $(echo "x $land"|./users); do
+		echo "you hit $a."
+		wall "$NAME hit $a with a water balloon"
+	done
+}
